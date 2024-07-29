d417bd2c409829e7934d9078472de6b7b47f3f73
aalgharibawi@gmail.com <aalgharibawi@gmail.com>
2024-07-29 15:12:34 -0400

    change the business name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
               className=" pb-16 text-6xl font-thin tracking-tight lg:mt-16
                     lg:text-6xl"
             >
-              Ali Al-gharibawi
+              Ali Dev
             </motion.h1>
             <motion.span
               variants={container(0.5)}
